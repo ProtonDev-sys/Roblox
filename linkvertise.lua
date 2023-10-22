b38f976c6b1f528e276dfef25b4da53084d66b75
ProtonDev-sys <72932384+ProtonDev-sys@users.noreply.github.com>
2023-10-22 17:29:22 +0100

    Update linkvertise.lua

--- linkvertise.lua
+++ linkvertise.lua
@@ -1,75 +1,19 @@
-local Title = "MikeCash - Please visit the website."
-
-local API_HOST = "s1.wayauth.com" -- s1.wayauth.com, s2.wayauth.com, s3.wayauth.com, s4.wayauth.com, s5.wayauth.com
-local LINKVERTISE_ID = 942203 -- Change me
-local LINKVERTISE_COUNT = 1 -- Change me
-local TOKEN_EXPIRE_TIME = 3600 -- Seconds
-
-local Task = {}
-Task.__index = Task
-
+local Title = "Generic Key - Key System"
+local ProjectId = "4620d413fa24"
 local HttpService = game:GetService("HttpService")
-local SHA2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/Egor-Skriptunoff/pure_lua_SHA/master/sha2.lua"))()
 local Iris = loadstring(game:HttpGet("https://raw.githubusercontent.com/x0581/Iris-Exploit-Bundle/main/bundle.lua"))().Init(game.CoreGui)
 
-function Task.new(API_HOST, LinkvertiseID, LinkCount, TokenExpireTime)
-    local nTask = {}
-
-    nTask.API_HOST = API_HOST or "s1.wayauth.com"
-    nTask.LinkvertiseID = LinkvertiseID or 12345
-    nTask.LinkCount = LinkCount or 1
-    nTask.Validator = tostring(math.random() + math.random(1, 100000) + Random.new():NextNumber())
-    nTask.TokenExpireTime = TokenExpireTime or 0
-
-    return setmetatable(nTask, Task)
-end
-
-function Task:create()
-    local URLBase = "http://%s/v2/create/%s/%s/%s"
-    local URL = URLBase:format(self.API_HOST, self.LinkvertiseID, self.Validator, self.LinkCount)
-    self.task = HttpService:JSONDecode(game:HttpGet(URL))
-    return self.task
-end
-
-function Task:verify()
-    local URLBase = "http://%s/v2/verify/%s/%s/%s"
-    local URL = URLBase:format(self.API_HOST, self.task.id, self.Validator, self.TokenExpireTime)
-    local Response = HttpService:JSONDecode(game:HttpGet(URL))
-    self.data = Response
-    if Response.success then
-        if SHA2.sha256(self.Validator):upper() == Response.validator:upper() then
-            return true
-        end
-    end
-    return false
-end
-
-function Task:getURL()
-    local URLBase = "http://%s/v2/wait/%s"
-    local URL = URLBase:format(self.API_HOST, self.task.id)
-    return URL
-end
-
-function Task:copyURL()
-    return setclipboard(self:getURL())
-end
-
-local nTask = Task.new(API_HOST, LINKVERTISE_ID, LINKVERTISE_COUNT, TOKEN_EXPIRE_TIME); nTask:create()
 local Verified = false
-
 Iris:Connect(function()
     if not Verified then
-        Iris.Window({Title, [Iris.Args.Window.NoClose] = true, [Iris.Args.Window.NoResize] = true, [Iris.Args.Window.NoScrollbar] = true, [Iris.Args.Window.NoCollapse] = true}, {size = Iris.State(Vector2.new(700, 60))}) do
-            Iris.SameLine() do
-                if Iris.Button({"I have visited the website. "..nTask:getURL()}).clicked then
-                    task.spawn(function()
-                        Verified = nTask:verify()
-                    end)
-                end
-                if Iris.Button({"Copy Website"}).clicked then
-                    nTask:copyURL()
-                end
-                Iris.End()
+        Iris.Window({Title, [Iris.Args.Window.NoClose] = true, [Iris.Args.Window.NoResize] = true, [Iris.Args.Window.NoScrollbar] = true, [Iris.Args.Window.NoCollapse] = true}, {size = Iris.State(Vector2.new(500, 120))}) do
+            local Key = Iris.InputText({"", [Iris.Args.InputText.TextHint] = "Your key here."}).text.value
+            if Iris.Button({"Check key"}).clicked then
+              task.spawn(function()
+                local Response = game:HttpGet(("https://mikecash.co/api/keyverify?key=%s&project=%s"):format(Key, ProjectId))
+                local Decode = HttpService:JSONDecode(Response)
+                Verified = Decode.success
+              end)
             end
             Iris.End()
         end
@@ -77,8 +21,3 @@ Iris:Connect(function()
 end)
 
 repeat task.wait() until Verified
-
-warn("Verified website at", nTask.data.timestamp)
-warn("Finished at", os.date("%c"))
-warn("Authentication Token", nTask.data.token)
-warn("Authentication Token Expire Time", TOKEN_EXPIRE_TIME)
